20f57bfd2241fd964b9bca0641fb0afd701c386d
eushakova1 <euhshakova1@eqvantalab.ru>
2025-01-31 16:27:49 +0700

    change calc for length_of_longest_substring

--- calc_longest_substring_without_repeating_characters.py
+++ calc_longest_substring_without_repeating_characters.py
@@ -25,30 +25,22 @@ Constraints:
 s consists of English letters, digits, symbols and spaces.
 """
 
-s = "pwwkewg"
+s = "pwwkewrs"
 
 
 def length_of_longest_substring(s):
     len_ss = 0
-    max_len = 0
-    list_ss = []
-    for count, i in enumerate(s, start=0):
-        if count == 0:
-            len_ss += 1
-        elif i != list_ss[count - 1] and i not in list_ss[count - len_ss : count + 1]:
-            len_ss += 1
+    max_len_ss = 0
+    seen = {}
+    for r in range(len(s)):
+        char = s[r]
+        if char in seen and seen[char] >= len_ss:
+            len_ss = seen[char] + 1
         else:
-            len_ss = 1
-        list_ss.append(i)
+            max_len_ss = max(len_ss, r - len_ss + 1)
+        seen[char] = r
 
-        if max_len < len_ss:
-            max_len = len_ss
-
-        print(count, count - len_ss)
-        print(len_ss)
-        print(list_ss)
-
-    return max_len
+    return max_len_ss
 
 
 result = length_of_longest_substring(s)
